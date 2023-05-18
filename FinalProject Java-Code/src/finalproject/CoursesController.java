c2c2fe76f1e127627fe5f4cf3a5ec151bf679878
engahmedalhopi <ahmedalhopi991@gmail.com>
2023-05-18 20:24:01 +0300

    18-5-2023 8:23 PM

---
+++ FinalProject Java-Code/src/finalproject/CoursesController.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author IT
+ */
+public class CoursesController implements Initializable {
+
+    @FXML
+    private TableView<ObservableList<String>> tableView;
+
+    @FXML
+    private TableColumn<ObservableList<String>, String> code;
+    @FXML
+    private TableColumn<ObservableList<String>, String> course;
+    @FXML
+    private TableColumn<ObservableList<String>, String> subject;
+    @FXML
+    private TableColumn<ObservableList<String>, String> book;
+    @FXML
+    private TableColumn<ObservableList<String>, String> number_lecture;
+    @FXML
+    private TableColumn<ObservableList<String>, String> teacher;
+    @FXML
+    private TableColumn<ObservableList<String>, String> place;
+    @FXML
+    private TableColumn<ObservableList<String>, String> procedure;
+    
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+    
+    public void get_courses() throws ClassNotFoundException{
+        PreparedStatement pst;
+        ResultSet rs = null;
+        Connection conn;
+
+        String sel = "select * from mang.courses";
+        try {
+            conn = DatabaseConnect.connDB();
+            pst = conn.prepareStatement(sel);
+            rs = pst.executeQuery();
+            ObservableList<ArrayList> data = FXCollections.observableArrayList();
+            while (rs.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                row.add(rs.getString("course_code"));
+                row.add(rs.getString("name"));
+                row.add(rs.getString("subject"));
+                row.add(rs.getString("book"));
+                row.add(rs.getString("number_lectures"));
+                row.add(rs.getString("teacher"));
+                row.add(rs.getString("place"));
+//                row.add(rs.getString("procedure"));
+                data.add(row);
+            }
+            code.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(0)));
+            course.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(1)));
+            subject.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(2)));
+            book.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(3)));
+            number_lecture.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(4)));
+            teacher.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(5)));
+            place.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(6)));
+            tableView.setItems(data);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    };
+    
+    public void add_course(){
+        
+    };
+    
+}
--- FinalProject Java-Code/src/finalproject/LoginController.java
+++ FinalProject Java-Code/src/finalproject/LoginController.java
@@ -1,5 +1,6 @@
 package finalproject;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,16 +9,21 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 
-
 public class LoginController implements Initializable {
-
 
     @FXML
     private TextField id_number_txt;
@@ -25,7 +31,9 @@ public class LoginController implements Initializable {
     private PasswordField password_txt;
     @FXML
     private CheckBox admin_login;
-    
+    @FXML
+    private Button login_btn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
@@ -34,52 +42,71 @@ public class LoginController implements Initializable {
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }    
-    
-    public void loginButtonClicked() throws ClassNotFoundException{
+        id_number_txt.setText("M001");
+        password_txt.setText("m001");
+        admin_login.setSelected(true);
+    }
+
+    public void loginButtonClicked(ActionEvent actionEvent) throws ClassNotFoundException, IOException {
         Connection conn = DatabaseConnect.connDB();
         System.out.println(conn);
-        PreparedStatement pst ;
-        ResultSet rs ;
+        PreparedStatement pst;
+        ResultSet rs;
         boolean found = false;
         boolean isAdmin = false;
         String log = "";
-        if(admin_login.isSelected()){
+        if (admin_login.isSelected()) {
             log = "select * from mang.manager where manager_number = ? and password = ?";
-        try {
-            pst = conn.prepareStatement(log);
-            pst.setString(1, id_number_txt.getText());
-            pst.setString(2, password_txt.getText());
-            rs = pst.executeQuery();
-            if (rs.next()) {
-                found = true;
-                isAdmin = true;
+            try {
+                pst = conn.prepareStatement(log);
+                pst.setString(1, id_number_txt.getText());
+                pst.setString(2, password_txt.getText());
+                rs = pst.executeQuery();
+                if (rs.next()) {
+                    found = true;
+                    isAdmin = true;
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
             }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e);
-        }
-        }else{
+        } else {
             log = "select * from mang.assistants where assistant_number = ? and password = ?";
-        try {
-            pst = conn.prepareStatement(log);
-            pst.setString(1, id_number_txt.getText());
-            pst.setString(2, password_txt.getText());
-            rs = pst.executeQuery();
-            if (rs.next()) {
-                found = true;
+            try {
+                pst = conn.prepareStatement(log);
+                pst.setString(1, id_number_txt.getText());
+                pst.setString(2, password_txt.getText());
+                rs = pst.executeQuery();
+                if (rs.next()) {
+                    found = true;
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+                System.out.println(e);
             }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e);
-            System.out.println(e);
         }
-        }
-        
-        if(found && isAdmin)
+
+        if (found && isAdmin) {
             System.out.println("Login Don Admin");
-        else if(found && !isAdmin)
+            Stage primaryStage = new Stage();
+            Stage currentStage = (Stage) login_btn.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminHome.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Attendance Management");
+            primaryStage.setResizable(false);
+            primaryStage.iconifiedProperty();
+            Image icon = new Image(getClass().getResourceAsStream("img/icon.jpg"));
+            primaryStage.getIcons().add(icon);
+            currentStage.close();
+            primaryStage.show();
+
+        } else if (found && !isAdmin) {
             System.out.println("Login Don User");
-        else
+        } else {
             JOptionPane.showMessageDialog(null, "Id Number or Password is not corrcet!");
-    };
-    
+        }
+    }
+;
+
 }
