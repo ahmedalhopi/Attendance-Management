77bc5dd18a2d31d04ce2ce1bdf6a5959ae9e7f35
engahmedalhopi <ahmedalhopi991@gmail.com>
2023-05-18 12:25:56 +0300

    18-5-2023 12:25

--- FinalProject Java-Code/src/finalproject/LoginController.java
+++ FinalProject Java-Code/src/finalproject/LoginController.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
@@ -13,11 +15,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.CheckBox;
 import javax.swing.JOptionPane;
 
-/**
- * FXML Controller class
- *
- * @author IT
- */
+
 public class LoginController implements Initializable {
 
 
@@ -30,7 +28,12 @@ public class LoginController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            Connection conn = DatabaseConnect.connDB();
+            System.out.println(conn);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
     public void loginButtonClicked() throws ClassNotFoundException{
@@ -39,9 +42,24 @@ public class LoginController implements Initializable {
         PreparedStatement pst ;
         ResultSet rs ;
         boolean found = false;
+        boolean isAdmin = false;
         String log = "";
         if(admin_login.isSelected()){
-            log = "select * from manager where manager_number = ? and password = ?";
+            log = "select * from mang.manager where manager_number = ? and password = ?";
+        try {
+            pst = conn.prepareStatement(log);
+            pst.setString(1, id_number_txt.getText());
+            pst.setString(2, password_txt.getText());
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                found = true;
+                isAdmin = true;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        }else{
+            log = "select * from mang.assistants where assistant_number = ? and password = ?";
         try {
             pst = conn.prepareStatement(log);
             pst.setString(1, id_number_txt.getText());
@@ -52,24 +70,14 @@ public class LoginController implements Initializable {
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e);
-        }
-        }else{
-            log = "select * from assistants where assistant_number = ? and password = ?";
-        try {
-            pst = conn.prepareStatement(log);
-            pst.setString(1, id_number_txt.getText());
-            pst.setString(2, password_txt.getText());
-            rs = pst.executeQuery();
-            if (!rs.wasNull()) {
-                found = true;
-            }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e);
+            System.out.println(e);
         }
         }
         
-        if(found)
-            System.out.print("Login Don");
+        if(found && isAdmin)
+            System.out.println("Login Don Admin");
+        else if(found && !isAdmin)
+            System.out.println("Login Don User");
         else
             JOptionPane.showMessageDialog(null, "Id Number or Password is not corrcet!");
     };
