a83e47a632d6ad6e06bf16c7c0ee7295377bfaa6
engahmedalhopi <ahmedalhopi991@gmail.com>
2023-05-22 13:16:49 +0300

    22-5-2023 1:16 PM

--- FinalProject Java-Code/src/finalproject/CoursesController.java
+++ FinalProject Java-Code/src/finalproject/CoursesController.java
@@ -24,6 +24,7 @@ import javafx.scene.control.TextFormatter;
 import javax.swing.JOptionPane;
 import javafx.scene.layout.Pane;
 
+
 public class CoursesController implements Initializable {
 
     @FXML
